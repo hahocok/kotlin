eca91066825a4ed088bcddc2fa6707d8c27f4986
Denis <hahocok@gmail.com>
2020-11-18 09:54:49 +0300

    Revert "добавил lesson 2"
    This reverts commit 909223ea

--- app/src/main/java/com/android/kotlin/data/model/Note.kt
+++
@@ -1,3 +0,0 @@
-package com.android.kotlin.data.model
-
-class Note(val title: String, val note: String, val color: Int)
--- app/src/main/java/com/android/kotlin/ui/main/MainViewState.kt
+++
@@ -1,5 +0,0 @@
-package com.android.kotlin.ui.main
-
-import com.android.kotlin.data.model.Note
-
-class MainViewState(val notes: List<Note>)
